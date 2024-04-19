aeccb8f82bc7dbcb8d918c36d8f084dbda147a6e
RGNGIT <vnifedow@bk.ru>
2024-04-19 19:16:59 +0500

    Микрофикс

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -46,7 +46,7 @@ export class UserController {
     const user = await this.userService.create(checkedConfirmUser.UserData);
 
     const roleDto = new DefineUserRoleDto();
-    roleDto.role_id = (await this.roleService.getRoleByName('portal_admin')).id;
+    roleDto.role_id = (await this.roleService.getRoleByName('Студент')).id;
     roleDto.user_id = user.id;
 
     await this.defineRole(roleDto);
