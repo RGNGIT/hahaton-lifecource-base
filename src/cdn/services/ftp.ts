e174ecc4f3169d4bded59e1e3d139c42bdb45a0f
RGNGIT <vnifedow@bk.ru>
2024-04-20 15:45:47 +0500

    Possible fix

--- src/cdn/services/ftp.ts
+++ src/cdn/services/ftp.ts
@@ -1,15 +1,16 @@
 import FtpClient from 'ftp';
 import CONST from '../const/constants';
+import { cdnConfig } from 'src/config';
 
 require('dotenv').config();
 const fs = require('fs');
 
 class FtpService {
   private config = {
-    host: process.env.CDN_HOST,
-    port: process.env.CDN_PORT,
-    user: process.env.CDN_USERNAME,
-    password: process.env.CDN_PASSWORD
+    host: cdnConfig.host,
+    port: cdnConfig.port,
+    user: cdnConfig.user,
+    password: cdnConfig.password
   };
   private client = new FtpClient();
   public async upload(key) {
