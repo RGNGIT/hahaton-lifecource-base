1fbea31f2d0d116a6a0f848ea4a2f21fb84cbe81
Julia <74661905+SunDust23@users.noreply.github.com>
2024-04-20 14:02:56 +0500

    сваггер

--- src/vacancies/dto/update-vacancy.dto.ts
+++ src/vacancies/dto/update-vacancy.dto.ts
@@ -1,6 +1,7 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateVacancyDto } from './create-vacancy.dto';
 
 export class UpdateVacancyDto extends PartialType(CreateVacancyDto) {
+  @ApiProperty()
   subscriber_id: number;
 }
