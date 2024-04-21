6dbfa2bcde09a6bb02d3acb5f5f53488644f6f0c
Julia <74661905+SunDust23@users.noreply.github.com>
2024-04-21 14:55:00 +0500

    комменты

--- src/publication/publication.module.ts
+++ src/publication/publication.module.ts
@@ -4,10 +4,12 @@ import { PublicationController } from './controllers/publication.controller';
 import { publicationProvider } from './providers/publication.provider';
 import { SequelizeModule } from 'src/sequelize/sequelize.module';
 import { FindService } from 'src/common/filters/find.service';
+import { CommentController } from './controllers/comment.controller';
+import { CommentService } from './services/comment.service';
 
 @Module({
   imports: [SequelizeModule],
-  controllers: [PublicationController],
-  providers: [PublicationService, FindService, ...publicationProvider],
+  controllers: [PublicationController, CommentController],
+  providers: [PublicationService, CommentService, FindService, ...publicationProvider],
 })
 export class PublicationModule {}
